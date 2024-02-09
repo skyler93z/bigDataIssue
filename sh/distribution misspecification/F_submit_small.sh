1cef7998b0ebd3feabdb788863e2fb42e2f03fa5
skyler93z <144880950+skyler93z@users.noreply.github.com>
2024-02-08 20:34:25 -0800

    Delete sh directory

--- sh/distribution misspecification/F_submit_small.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -J F_small
-#SBATCH --account=def-ubcxzh
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G 
-#SBATCH --time=0-23:59 # running time
-cd /home/skyler93/projects/def-ubcxzh/skyler93/big_sample_issue_final
-module load StdEnv/2023 r/4.3.1
-Rscript F_possion_per_small.R $1
