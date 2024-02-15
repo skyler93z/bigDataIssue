4ba52a1cb73202c1ec78e099268569ac90b93595
skyler93z <144880950+skyler93z@users.noreply.github.com>
2024-02-14 20:44:44 -0800

    Update PoisBias.R

--- PoisBias/code/PoisBias.R
+++ PoisBias/code/PoisBias.R
@@ -13,7 +13,6 @@ sizes <- 10^seq(1,6,length.out=mm)
 bias <- matrix(NA, mm,ll)
 for(ii in 1:mm) for(jj in 1:ll) bias[ii,jj] = mean(simPois2(sizes[ii],lambda))-lambda
 
-#setwd("/Users/xzhang/Library/CloudStorage/Dropbox/Research Projects/BigSampleIssue")
 png(file = './PoisBias/figure/Figure1.png', width=1400, height=480)
 par(mar = c(4.1, 8, 0.1, 0.1))
 boxplot(t(bias),horizontal=T,pch=".",ylim=c(-0.3,0.3), yaxt="n", xlab=expression(paste("Bias of estimation parameter ",lambda)), cex.lab=2,cex.axis=2)
@@ -23,4 +22,3 @@ abline(v=0,col=2, lwd=3,lty=2)
 dev.off()
 
 
-#plot(log(sizes)/log(10), apply(bias,1, median))
