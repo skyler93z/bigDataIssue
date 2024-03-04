3f5933ce8fac8961e69b6f2f47777a97064ce0ea
Soojeong Kim <123779328+Soojeong0519@users.noreply.github.com>
2024-03-04 12:07:29 -0800

    Update problem.sh

--- scenario2/sh/problem.sh
+++ scenario2/sh/problem.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem=40g
 #SBATCH --time=3:00:00
 module load StdEnv/2023 r/4.3.1
-Rscript ./scenario2_new/code/problem.R $1 $2 > ./scenario2_new/rout/problem.$1.$2.Rout 2>&1
+Rscript ./scenario2/code/problem.R $1 $2 > ./scenario2/rout/problem.$1.$2.Rout 2>&1
